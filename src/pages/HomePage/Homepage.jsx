510ecfa5fe3b9c33208cc441a84bfb251e6338b6
Olena457 <olenademianenko42@gmail.com>
2025-03-14 20:38:55 +0200

    added main page

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,15 +1,19 @@
 import css from './HomePage.module.css';
+
 const HomePage = () => {
-  <div className="home">
-    <div className={css.content}></div>
-    <h1 className={css.title}>
-      Take good<span className={css.light}>care</span> of your small pets
-    </h1>
-    <p className={css.description}>
-      Choosing a pet for your home is a choice that is meant to enrich your life
-      with immeasurable joy and tenderness.
-    </p>
-    <div className={css.photo}></div>
-  </div>;
+  return (
+    <div className={css.home}>
+      <div className={css.content}></div>
+      <h1 className={css.title}>
+        Take good<span className={css.light}>care</span> of your small pets
+      </h1>
+      <p className={css.description}>
+        Choosing a pet for your home is a choice that is meant to enrich your
+        life with immeasurable joy and tenderness.
+      </p>
+      <div className={css.photo}></div>
+    </div>
+  );
 };
+
 export default HomePage;
