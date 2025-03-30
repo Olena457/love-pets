11c2f1a36e52efb6a7358172e29b26412924208e
Olena457 <olenademianenko42@gmail.com>
2025-03-30 19:12:47 +0300

    adde container component and style home header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,3 @@
-// import { useLocation } from 'react-router-dom';
-
-// import LogoChange from '../LogoChange/LogoChange.jsx';
-// import NavBar from '../NavBar/NavBar.jsx';
-// import css from './Header.module.css';
-// const Header = () => {
-//   const location = useLocation();
-//   const isHomePage = location.pathname === './home';
-//   return (
-//     <header className={`${css.header} ${isHomePage ? css.homeHeader : ''}`}>
-//       <LogoChange />
-//       <NavBar />
-//     </header>
-//   );
-// };
-
-// export default Header;
 import { useLocation } from 'react-router-dom';
 import LogoChange from '../LogoChange/LogoChange.jsx';
 import NavBar from '../NavBar/NavBar.jsx';
--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,21 +1,25 @@
+import Container from '../../components/Container/Container.jsx';
 import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div className={css.wrapper}>
-      <div className={css.home}>
-        <div className={css.content}>
-          <h1 className={css.title}>
-            Take good<span className={css.light}>care</span> of your small pets
-          </h1>
-          <p className={css.description}>
-            Choosing a pet for your home is a choice that is meant to enrich
-            your life with immeasurable joy and tenderness.
-          </p>
+    <Container>
+      <div className={css.wrapper}>
+        <div className={css.home}>
+          <div className={css.content}>
+            <h1 className={css.title}>
+              Take good<span className={css.light}>care</span> of your small
+              pets
+            </h1>
+            <p className={css.description}>
+              Choosing a pet for your home is a choice that is meant to enrich
+              your life with immeasurable joy and tenderness.
+            </p>
+          </div>
         </div>
+        <div className={css.photo}></div>
       </div>
-      <div className={css.photo}></div>
-    </div>
+    </Container>
   );
 };
 
