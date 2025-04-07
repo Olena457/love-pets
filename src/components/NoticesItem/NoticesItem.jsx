8e4a3b3a982e3956f65aa2116e0add96c9e078bf
Olena457 <olenademianenko42@gmail.com>
2025-04-07 22:42:03 +0300

    added modal logic

--- src/components/NoticesItem/NoticesItem.jsx
+++ src/components/NoticesItem/NoticesItem.jsx
@@ -1,98 +1,139 @@
-import css from './NoticesItem.module.css';
-import heartFull from '../../assets/icons/heartEmpty.svg';
-import heartEmpty from '../../assets/icons/heartFull.svg';
-import starYellow from '../../assets/icons/starYellow.svg';
+// import { useDispatch,useSelector } from 'react-redux';
+// import {
+//   fetchNoticeById,
+//   addNoticeToFavorites,
+//   removeNoticeFromFavorites,
+//   toggleFavoriteNotice,
+// } from '../../redux/notices/noticesOperations.js';
+// import { selectIsAuthenticated } from '../../redux/users/usersSelectors.js';
+// import { openModal } from '../../redux/modal/modalSlice.js';
+// import { fetchProfile } from '../../redux/profile/profileSlice.js';
 
-const NoticesItem = ({ notice, onLearnMore, onToggleFavorite }) => {
-  const {
-    _id,
-    imgURL,
-    title,
-    popularity,
-    name,
-    birthday,
-    sex,
-    species,
-    category,
-    comment,
-    price = 0,
-    isFavorite,
-  } = notice;
+// import css from './NoticesItem.module.css';
+// import heartFull from '../../assets/icons/heartEmpty.svg';
+// import heartEmpty from '../../assets/icons/heartFull.svg';
+// import starYellow from '../../assets/icons/starYellow.svg';
 
-  const handleLearnMore = () => {
-    onLearnMore(_id);
-  };
+// const NoticesItem = ({ notice, onLearnMore, onToggleFavorite }) => {
+//   const dispatch = useDispatch();
+//   const isAuthenticated = useSelector(selectIsAuthenticated);
 
-  const handleToggleFavorite = () => {
-    onToggleFavorite(_id);
-  };
+//   const {
+//     _id,
+//     imgURL,
+//     title,
+//     popularity,
+//     name,
+//     birthday,
+//     sex,
+//     species,
+//     category,
+//     comment,
+//     price = 0,
+//     isFavorite,
+//   } = notice;
 
-  return (
-    <div className={css.item}>
-      <img src={imgURL} alt={title} className={css.image} />
-      <div className={css.titleContainer}>
-        <h2 className={css.title}>{title}</h2>
-        <div className={css.popularity}>
-          <img
-            src={starYellow}
-            alt="star"
-            width="24"
-            height="24"
-            className={css.star}
-          />
-          <span>{popularity}</span>
-        </div>
-      </div>
-      <div className={css.infoContainer}>
-        <p>
-          <b className={css.info}>Name:</b> {name}
-        </p>
-        <p>
-          <b className={css.info}>Birthday:</b>{' '}
-          {birthday
-            ? new Date(birthday).toLocaleDateString('uk-UA')
-            : 'Unknown'}
-        </p>
-        <p>
-          <b className={css.info}>Sex:</b> {sex}
-        </p>
-        <p>
-          <b className={css.info}>Species:</b> {species}
-        </p>
-        <p>
-          <b className={css.info}>Category:</b> {category}
-        </p>
-      </div>
-      <p className={css.comment}>{comment}</p>
-      <p className={css.price}>
-        {price > 0 ? `$${price}` : 'Price negotiable'}
-      </p>
-      <div className={css.actions}>
-        <button className={css.learnMore} onClick={handleLearnMore}>
-          Learn more
-        </button>
-        <button className={css.favorite} onClick={handleToggleFavorite}>
-          {isFavorite ? (
-            <img
-              src={heartEmpty}
-              alt="heart"
-              width="24"
-              height="24"
-              className={css.heart}
-            />
-          ) : (
-            <img
-              src={heartFull}
-              alt="heart"
-              width="24"
-              height="24"
-              className={css.heart}
-            />
-          )}
-        </button>
-      </div>
-    </div>
-  );
-};
+//   const handleLearnMore = () => {
+//     if (!isAuthenticated) {
+//       dispatch(openModal());
+//       dispatch(fetchNoticeById({ _id: notice._id }));
 
-export default NoticesItem;
+//     }
+//     dispatch((openModal()));
+
+//     const handleToggleFavorite = () => {
+//       if (!isAuthenticated) {
+//         dispatch(openModal());
+//       }
+//       dispatch(toggleFavoriteNotice({ _id: notice._id }));
+//   };
+//     const hendleRemoveNotices = async () => {
+//       if (!isAuthenticated) {
+//         dispatch(openModal());
+//       }
+//       try {
+//         await dispatch(removeNoticeFromFavorites(_id));
+//         dispatch(fetchProfile());
+//       } catch (error) {
+//         console.error('Error removing notice from favorites:', error);
+//       }
+//     }
+
+// }
+//   return (
+//     <div className={css.item}>
+//       <img src={imgURL} alt={title} className={css.image} />
+//       <div className={css.titleContainer}>
+//         <h2 className={css.title}>{title}</h2>
+//         <div className={css.popularity}>
+//           <img
+//             src={starYellow}
+//             alt="star"
+//             width="24"
+//             height="24"
+//             className={css.star}
+//           />
+//           <span>{popularity}</span>
+//         </div>
+//       </div>
+//       <div className={css.infoContainer}>
+//         <p>
+//           <b className={css.info}>Name:</b> {name}
+//         </p>
+//         <p>
+//           <b className={css.info}>Birthday:</b>{' '}
+//           {birthday
+//             ? new Date(birthday).toLocaleDateString('uk-UA')
+//             : 'Unknown'}
+//         </p>
+//         <p>
+//           <b className={css.info}>Sex:</b> {sex}
+//         </p>
+//         <p>
+//           <b className={css.info}>Species:</b> {species}
+//         </p>
+//         <p>
+//           <b className={css.info}>Category:</b> {category}
+//         </p>
+//       </div>
+//       <p className={css.comment}>{comment}</p>
+//       <div className={css.priceContainer}>
+//         {price > 0 ? (
+//           <span className={css.price}>${price}</span>
+//         ) : (
+//           <span className={css.negotiable}>Price negotiable</span>
+//         )}
+//       </div>
+
+//       {/* <p className={css.price}>
+//         {price > 0 ? `$${price}` : 'Price negotiable'}
+//       </p> */}
+//       <div className={css.actions}>
+//         <button className={css.learnMore} onClick={handleLearnMore}>
+//           Learn more
+//         </button>
+//         <button className={css.favorite} onClick={handleToggleFavorite}>
+//           {isFavorite ? (
+//             <img
+//               src={heartEmpty}
+//               alt="heart"
+//               width="24"
+//               height="24"
+//               className={css.heart}
+//             />
+//           ) : (
+//             <img
+//               src={heartFull}
+//               alt="heart"
+//               width="24"
+//               height="24"
+//               className={css.heart}
+//             />
+//           )}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default NoticesItem;
--- src/redux/users/usersSelectors.js
+++ src/redux/users/usersSelectors.js
@@ -2,4 +2,4 @@ export const selectUser = state => state.users.user;
 export const selectToken = state => state.users.token;
 export const selectIsLoading = state => state.users.isLoading;
 export const selectError = state => state.users.error;
-export const selectIsAuthenticated = state => Boolean(state.users.toket);
+export const selectIsAuthenticated = state => Boolean(state.users.token);
