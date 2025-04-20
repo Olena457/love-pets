ded5729b36fe10b3097fce79ad047a71bb8e3205
Olena457 <olenademianenko42@gmail.com>
2025-04-20 22:21:28 +0300

    fixed main page

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,18 @@
 import css from './MainPage.module.css';
 import { Link } from 'react-router';
+import heartFull from '../../assets/icons/heartFull.svg';
 const MainPage = () => {
   return (
     <div className={css.mainContainer}>
       <div className={css.containerLogo}>
-        <Link to="/home" className="homeButton">
-          <div className={css.logoMain}></div>
+        <Link to="/home" className={css.homeButton}>
+          <div className={css.logoMain}>
+            <div className={css.logoText}>
+              Petl
+              <img className={css.logoIcon} src={heartFull} alt="Heart Icon" />
+              ve
+            </div>
+          </div>
         </Link>
       </div>
     </div>
