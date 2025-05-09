b5b92cbf0987c939393265a141d6938ad34cf235
Olena457 <olenademianenko42@gmail.com>
2025-05-10 00:33:23 +0300

    fixed user nav

--- src/components/UserNav/UserNav.jsx
+++ src/components/UserNav/UserNav.jsx
@@ -1,97 +1,89 @@
-// import { useLocation } from 'react-router-dom';
+// import { useEffect, useState } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
-// import { useState } from 'react';
-// import { signout } from '../../redux/users/usersOperations.js';
-// import ModalApproveAction from '../ModalApproveAction/ModalApproveAction.jsx';
 // import { selectUser } from '../../redux/users/usersSelectors.js';
+// import { useLocation } from 'react-router-dom';
+// import { closeMobMenu } from '../../redux/mobile/mobMenuSlice.js';
+// import { openApproveModal } from '../../redux/modal/modalSlice.js';
 // import css from './UserNav.module.css';
 
-// const UserNav = ({ onClose }) => {
-//   const location = useLocation();
-//   const isHomePage = location.pathname === '/home';
-//   const dispatch = useDispatch();
-//   const [isModalOpen, setIsModalOpen] = useState(false);
+// const UserNav = () => {
 //   const user = useSelector(selectUser);
+//   const location = useLocation();
+//   const dispatch = useDispatch();
+//   const [isHomePage, setIsHomePage] = useState(false);
+
+//   useEffect(() => {
+//     switch (location.pathname) {
+//       case '/home':
+//         setIsHomePage(true);
+//         break;
+
+//       default:
+//         setIsHomePage(false);
+//         break;
+//     }
+//   }, [location.pathname]);
 
 //   const handleLogout = () => {
-//     dispatch(signout());
-//     if (onClose) {
-//       onClose();
-//     }
-//   };
-
-//   const openModal = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setIsModalOpen(false);
+//     dispatch(closeMobMenu());
+//     dispatch(openApproveModal());
 //   };
 
 //   return (
 //     <div className={`${css.authNav} ${isHomePage ? css.authNavHome : ''}`}>
-//       {user?.isRegistered && user?.isLoggedIn && (
-//         <>
-//           <button className={css.logoutButton} onClick={openModal}>
-//             LOG OUT
-//           </button>
-//           {isModalOpen && (
-//             <ModalApproveAction
-//               onConfirm={handleLogout}
-//               onCansel={closeModal}
-//               message="Already leaving?"
-//             />
-//           )}
-//         </>
+//       {user && (
+//         <button
+//           className={`${css.logoutButton} ${isHomePage ? css.homeStyle : ''}`}
+//           onClick={handleLogout}
+//         >
+//           LOG OUT
+//         </button>
 //       )}
 //     </div>
 //   );
 // };
-
 // export default UserNav;
-
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from '../../redux/users/usersSelectors.js';
 import { useLocation } from 'react-router-dom';
 import { closeMobMenu } from '../../redux/mobile/mobMenuSlice.js';
 import { openApproveModal } from '../../redux/modal/modalSlice.js';
+import { selectIsModalOpen } from '../../redux/modal/modalSelectors.js';
+import ModalApproveAction from '../ModalApproveAction/ModalApproveAction.jsx';
 import css from './UserNav.module.css';
 
 const UserNav = () => {
   const user = useSelector(selectUser);
+  const isModalOpen = useSelector(selectIsModalOpen);
   const location = useLocation();
   const dispatch = useDispatch();
-  const [isHome, setIsHome] = useState(false);
+  const [isHomePage, setIsHomePage] = useState(false);
 
   useEffect(() => {
-    switch (location.pathname) {
-      case '/home':
-        setIsHome(true);
-        break;
-
-      default:
-        setIsHome(false);
-        break;
-    }
+    setIsHomePage(location.pathname === '/home');
   }, [location.pathname]);
 
-  const handleLogout = () => {
+  const handleLogoutClick = () => {
     dispatch(closeMobMenu());
     dispatch(openApproveModal());
   };
 
   return (
-    <div className={`${css.authNav} ${isHome ? css.authNavHome : ''}`}>
-      {user && (
-        <button
-          className={`${css.logoutButton} ${isHome ? css.homeStyle : ''}`}
-          onClick={handleLogout}
-        >
-          LOG OUT
-        </button>
+    <div className={`${css.authNav} ${isHomePage ? css.authNavHome : ''}`}>
+      {user?.isRegistered && user?.isLoggedIn && (
+        <>
+          <button
+            className={`${css.logoutButton} ${isHomePage ? css.homeStyle : ''}`}
+            onClick={handleLogoutClick}
+          >
+            LOG OUT
+          </button>
+          {isModalOpen && <ModalApproveAction />}
+        </>
       )}
     </div>
   );
 };
+
 export default UserNav;
