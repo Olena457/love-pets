e4fd65689cf46ee688e83925dca9e0ec6c006e3c
Olena457 <olenademianenko42@gmail.com>
2025-05-30 17:12:30 +0300

    added redirects

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import { lazy, Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -91,6 +91,7 @@ function App() {
               <PrivateRoute redirectTo="/" component={<ProfilePage />} />
             }
           >
+            <Route index element={<Navigate to="favorites" replace />} />
             <Route path="favorites" element={<MyFavoritePets />} />
             <Route path="viewed" element={<Viewed />} />
           </Route>
